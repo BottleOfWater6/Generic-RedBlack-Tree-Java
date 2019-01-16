d2f138d0880f021f883a85ef48c88dc4e11fdc17
PranavB6 <b.pranav.k@gmail.com>
2019-01-15 22:07:08 -0700

    Finished node class, Added left and right rotate, started working on insertion

--- src/GenericRedBlackTree.java
+++ src/GenericRedBlackTree.java
@@ -1,3 +1,5 @@
+import org.omg.SendingContext.RunTime;
+
 /**
  * Lab 4: Generics <br />
  * The {@code GenericRedBlackTree} class <br />
@@ -6,20 +8,12 @@
  *            </a>
  */
 public class GenericRedBlackTree<K extends Comparable<K>, V> {
-    public static final boolean BLACK = false;
-    public static final boolean RED = true;
+    public final boolean BLACK = false;
+    public final boolean RED = true;
 
     private Node root = null;
-    private Node Nil = null;
     private int size = 0;
 
-    GenericRedBlackTree() {
-        Nil = new Node(null, null) {
-            @Override
-            public K key()      { throw new RuntimeException("Tried to access Nil key"); }
-            public V value()    { throw new RuntimeException("Tried to access Nil value"); }
-        };
-    }
 
     /**
      * Search for the node by key, and return the corresponding value
@@ -39,7 +33,98 @@ public class GenericRedBlackTree<K extends Comparable<K>, V> {
      */
     public void insert(K key, V value) {
 
+        if (root == null) {
+            root = new Node(key, value);
+            return;
+        }
+        
+        Node curr = root;
+        while (curr.key() != null) {
 
+            if (key.compareTo(curr.key()) > 0) {
+                curr = curr.right;
+
+            } else if (key.compareTo(curr.key()) < 0) {
+                curr = curr.left;
+
+            } else {
+                curr.setValue(value);
+                return;
+            }
+
+        }
+
+        curr.set(key, value);
+
+//        insertFixUp(curr);
+    }
+
+    private void insertFixUp(Node z) {
+
+        Node y;
+
+        while (z.parent.color == RED) {
+            if (z.parent.isLeft()) {
+
+                y = z.parent.parent.right;
+
+                if (y.color == RED) {
+                    z.parent.color = BLACK;
+                    y.color = BLACK;
+                    z.parent.parent.color = RED;
+                    z = z.parent.parent;
+
+                } else if (z.isRight()){
+                    z = z.parent;
+
+                }
+            }
+        }
+
+
+
+    }
+
+    private void leftRotate(Node x) {
+
+        Node y = x.right;
+        x.right = y.left;
+        y.left.parent = x;
+        y.parent = x.parent;
+
+        if (x.parent == null) {
+            root = y;
+
+        } else if (x.isLeft()) {
+            x.parent.left = y;
+
+        } else {
+            x.parent.right = y;
+        }
+
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rightRotate( Node x) {
+        Node y = x.left;
+        x.left = y.right;
+        y.right.parent = x;
+        y.parent = x.parent;
+
+        if (x.parent == null) {
+            root = y;
+
+        } else if (x.isRight()) {
+            x.parent.right = y;
+
+        } else {
+            x.parent.left = y;
+
+        }
+
+        y.right = x;
+        x.parent = y;
     }
 
     /**
@@ -67,9 +152,45 @@ public class GenericRedBlackTree<K extends Comparable<K>, V> {
      */
     @Override public String toString() {
         // TODO: Lab 4 Part 3-4 -- print the tree, where each node contains both value and color
-        // You can print it by in-order traversal
+
+
 
         return null;
+    }
+
+    public void print() {
+
+        printRecurse(root);
+
+        System.out.println("--------------- Left rotate root");
+
+        leftRotate(root.right);
+        rightRotate(root.right);
+
+        printRecurse(root);
+
+
+
+
+    }
+
+    public void printRecurse(Node node) {
+
+        System.out.println(node);
+
+        if (node.left != null) {
+            System.out.print("<L> ");
+            printRecurse(node.left);
+//            System.out.print("</L> ");
+        }
+
+        if (node.right != null) {
+            System.out.print("<R> ");
+            printRecurse(node.right);
+//            System.out.print("</R> ");
+
+        }
+
     }
 
     /**
@@ -77,56 +198,121 @@ public class GenericRedBlackTree<K extends Comparable<K>, V> {
      * @param args      {@code String[]} Command line arguments
      */
     public static void main(String[] args) {
-        GenericRedBlackTree<Integer, String> rbt = new GenericRedBlackTree<Integer, String>();
-        int[] keys = new int[10];
-        for (int i = 0; i < 10; i++) {
-            keys[i] = (int) (Math.random() * 200);
-            System.out.println(String.format("%2d Insert: %-3d ", i+1, keys[i]));
-            rbt.insert(keys[i], "\"" + keys[i] + "\"");
-        } // for (int i = 0; i < 10; i++)
 
-        assert rbt.root.color == GenericRedBlackTree.Node.BLACK;
-        System.out.println(rbt.root);                   // This helps to figure out the tree structure
-        System.out.println(rbt);
+        GenericRedBlackTree<Integer, String> rbt = new GenericRedBlackTree<>();
+//        rbt.insert(-5, "-5");
+        rbt.insert(0, "0");
+        rbt.insert(-1, "-1");
+        rbt.insert(1, "1");
+        rbt.insert(2, "2");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(String.format("%2d Delete: %3d(%s)", i+1, keys[i], rbt.remove(keys[i])));
-            if ((i + 1) % 5 == 0) {
-                System.out.println(rbt);
-            } // if ((i + 1) % 5 == 0)
-        } // for (int i = 0; i < 10; i++)
+        rbt.print();
+
+
+//        GenericRedBlackTree<Integer, String> rbt = new GenericRedBlackTree<Integer, String>();
+//        int[] keys = new int[10];
+//        for (int i = 0; i < 10; i++) {
+//            keys[i] = (int) (Math.random() * 200);
+//            System.out.println(String.format("%2d Insert: %-3d ", i+1, keys[i]));
+//            rbt.insert(keys[i], "\"" + keys[i] + "\"");
+//        } // for (int i = 0; i < 10; i++)
+//
+//        System.out.println(rbt.root);                   // This helps to figure out the tree structure
+//        System.out.println(rbt);
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(String.format("%2d Delete: %3d(%s)", i+1, keys[i], rbt.remove(keys[i])));
+//            if ((i + 1) % 5 == 0) {
+//                System.out.println(rbt);
+//            } // if ((i + 1) % 5 == 0)
+//        } // for (int i = 0; i < 10; i++)
     }
 
-    private class Node {
+    private class Node implements Comparable{
 
         private K key;
         private V value;
         public boolean color = BLACK;
-        public Node parent = null, left = Nil, right = Nil;
+
+        public Node parent = null;
+        public Node left = null;
+        public Node right = null;
 
         public Node(K key, V value) {                   // By default, a new node is black with two NIL children
             this.key = key;
             this.value = value;
 
-//            if (value != null) {
-//                lChild = new Node(null, null);          // And the NIL children are both black
-//                lChild.parent = this;
-//                rChild = new Node(null, null);
-//                rChild.parent = this;
-//            }
+            if (key != null) {
+                left = new Node(null, null);
+                left.parent = this;
+
+                right = new Node(null, null);
+                right.parent = this;
+            }
         }
 
         public K key()    { return key; }
         public V value()  { return value; }
 
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public boolean exists() {
+            return (this.key() != null);
+        }
+
+        public boolean isLeft() {
+            if (this.parent == null) {
+                throw new RuntimeException("Node has no parent :(");
+            }
+            return (this.parent.left == this);
+        }
+
+        public boolean isRight() {
+            if (this.parent == null) {
+                throw new RuntimeException("Node has no parent :(");
+            }
+            return (this.parent.right == this);
+        }
+
+        public void set(K key, V value) {
+            this.key = key;
+            this.value = value;
+
+            left = new Node(null, null);
+            left.parent = this;
+
+            right = new Node(null, null);
+            right.parent = this;
+
+            this.color = RED;
+
+
+        }
+
+        @Override
+        public int compareTo(Object other) {
+
+            try {
+                Node tmp = ((Node) other);
+                System.out.println("Comparing Node to Node");
+                return this.key().compareTo(((Node) other).key());
+
+            } catch (ClassCastException e) {
+                throw new RuntimeException("Comparing Node to non-Node");
+            }
+        }
+
         /**
          * Print the tree node: red node wrapped by "<>"; black node by "[]"
          * @return          {@code String} The printed string of the tree node
          */
-        @Override public String toString() {
-            if (value == null)
-                return "";
-            return (color == RED) ? "<" + value + "(" + key + ")>" : "[" + value + "(" + key + "]";
+        @Override
+        public String toString() {
+            if (key == null)
+                return "Nil";
+            return (color == RED) ? "<" + key + "," + value + ">" : "[" + key + "," + value + "]";
         }
     }
 
